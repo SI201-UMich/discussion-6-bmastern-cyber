ac4746ebccc44fe676a86805a5d81f16395f5e44
Braden Masternak <bradenmasternak@Bradens-MacBook-Air.local>
2026-02-18 18:25:57 -0500

    first commit

--- discussion6.py
+++ discussion6.py
@@ -57,7 +57,23 @@ class HorseRaces:
             inner keys are (str) races, inner values are (int) race times
             EXAMPLE: {'Special Week': {'Tenno Sho Fall': 16.5, 'Tenno Sho Spring': 16.3, 'Teio Sho': 17.0}}
         '''
-        pass
+        header = table[0]
+        items =table[1:]
+        output = {}
+
+        for item in items:
+            horse_name = item[0]
+            value_1 = item[1]
+            value_2 = item[2]
+            value_3 = item[3]
+            output[horse_name] = {header[1]: float(value_1),
+                                  header[2]: float(value_2),
+                                  header[3]: float(value_3)}
+        
+        return output
+
+
+        print(table)
 
 ###############################################################################
 ##### TASK 2
